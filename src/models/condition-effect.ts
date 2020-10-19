75f9b26bf85d16a7641785529ce816cc9d0b2d01
abrn <lordzhhao@gmail.com>
2020-10-19 22:59:45 +0100

    add new condition effect statdata

--- src/models/condition-effect.ts
+++ src/models/condition-effect.ts
@@ -7,7 +7,7 @@ export class ConditionEffects {
    */
   static has(condition: number, effect: ConditionEffect): boolean {
     // tslint:disable no-bitwise
-    const effectBit = 1 << effect - 1;
+    const effectBit = 1 << (effect - 1);
     return (condition & effectBit) === 1;
     // tslint:enable no-bitwise
   }
@@ -25,7 +25,7 @@ export class ConditionEffects {
  */
 export function hasEffect(condition: number, effect: ConditionEffect): boolean {
   // tslint:disable no-bitwise
-  const effectBit = 1 << effect - 1;
+  const effectBit = 1 << (effect - 1);
   return (condition & effectBit) === 1;
   // tslint:enable no-bitwise
 }
@@ -51,7 +51,7 @@ export enum ConditionEffect {
   PARALYZED = 14,
   SPEEDY = 15,
   BLEEDING = 16,
-  NOT_USED = 17,
+  ARMOR_BROKEN_IMMUNE = 17,
   HEALING = 18,
   DAMAGING = 19,
   BERSERK = 20,
@@ -64,4 +64,35 @@ export enum ConditionEffect {
   ARMORBROKEN = 27,
   HEXED = 28,
   NINJA_SPEEDY = 29,
+  UNSTABLE = 30,
+  DARKNESS = 31,
+  SLOWED_IMMUNE = 32,
+  DAZED_IMMUNE = 33,
+  PARALYZED_IMMUNE = 34,
+  PETRIFIED = 35,
+  PETRIFIED_IMMUNE = 36,
+  PET_EFFECT_ICON = 37,
+  CURSE = 38,
+  CURSE_IMMUNE = 39,
+  HP_BOOST = 40,
+  MP_BOOST = 41,
+  ATT_BOOST = 42,
+  DEF_BOOST = 43,
+  SPD_BOOST = 44,
+  VIT_BOOST = 45,
+  WIS_BOOST = 46,
+  DEX_BOOST = 47,
+  SILENCED = 48,
+  EXPOSED = 49,
+  ENERGIZED = 50,
+  HP_DEBUFF = 51,
+  MP_DEBUFF = 52,
+  ATT_DEBUFF = 53,
+  DEF_DEBUFF = 54,
+  SPD_DEBUFF = 55,
+  VIT_DEBUFF = 56,
+  WIS_DEBUFF = 57,
+  DEX_DEBUFF = 58,
+  INSPIRED = 59,
+  GROUND_DAMAGE = 99
 }
