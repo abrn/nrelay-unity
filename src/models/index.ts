efdc3c5c4408fa19f35cec8b565fb40f853c88d3
abrn <lordzhhao@gmail.com>
2020-10-22 07:46:14 +0100

    Update index.ts

--- src/models/index.ts
+++ src/models/index.ts
@@ -19,6 +19,7 @@ export * from './pet-data';
 export * from './playerdata';
 export * from './plugin-info';
 export * from './projectile';
+export * from './proxy-pool';
 export * from './proxy';
 export * from './server';
 export * from './socket-wrapper';
